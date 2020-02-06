56035d35eb601ceabf7191aba9280476c0019ae0
Phil <phillipchan1@gmail.com>
2020-02-06 11:28:53 -0800

    Try uploading wordpress images

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var ghost = require("ghost");
-var express = require("express");
-var urlService = require("./node_modules/ghost/core/frontend/services/url");
+var ghost = require('ghost');
+var express = require('express');
+var urlService = require('./node_modules/ghost/core/frontend/services/url');
 var parentApp = express();
 
+console.log('in server');
 // Run a single Ghost process
 ghost()
-  .then(function(ghostServer) {
-    // for making subdir work
-    parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
-    ghostServer.start(parentApp);
-  })
-  .catch(error => {
-    console.error(`Ghost server error: ${error.message} ${error.stack}`);
-    process.exit(1);
-  });
+	.then(function(ghostServer) {
+		// console.log(`TCL: ghostServer`, ghostServer.config);
+		// for making subdir work
+		parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
+		ghostServer.start(parentApp);
+	})
+	.catch(error => {
+		console.error(`Ghost server error: ${error.message} ${error.stack}`);
+		process.exit(1);
+	});
